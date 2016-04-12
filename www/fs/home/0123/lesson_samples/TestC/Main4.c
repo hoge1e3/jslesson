be47c131bf36bd6e024a38e4378b294cb3e13923
hoge1e3 <hoge1e3@gmail.com>
2016-04-12 12:09:51 +0900

    del home for ignore

--- www/fs/home/0123/lesson_samples/TestC/Main4.c
+++
@@ -1,6 +0,0 @@
-// C
-int main(void){
-    printf("hello world!!\n");
-    printf("konnichiwa sekai\n");
-    printf("%d",3+2);
-}
--- www/fs/home/0123/neko/ctest/Test.c
+++
@@ -1,6 +0,0 @@
-int main(void){
-    //int=3;
-    //a=3;
-    printf("%d",3+5);
-    
-}
