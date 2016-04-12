460cdca395544a9a6d837aa7f47bb6a861a2ddf2
hoge1e3 <hoge1e3@gmail.com>
2016-04-12 14:25:17 +0900

    concat FS.js (not work)

--- www/js/fs/fs2stub.js
+++ www/js/fs/fs2stub.js
@@ -1,29 +1,5 @@
-define(["FS2","WebSite","NativeFS","LSFS", "PathUtil","Env","assert","SFile","RootFS"],
-        function (FS,WebSite,NativeFS,LSFS, P,Env,A,SFile,RootFS) {
-    var FS={};
-    var rootFS;
-    var env=new Env(WebSite);
-    if (WebSite.isNW) {
-        var nfsp=process.env.TONYU_FS_HOME || P.rel(process.cwd().replace(/\\/g,"/"), "www/fs/");
-        console.log("nfsp",nfsp);
-        rootFS=new RootFS(new NativeFS(nfsp));
-    } else {
-        rootFS=new RootFS(new LSFS(localStorage));
-    }
-    FS.get=function () {
-        return rootFS.get.apply(rootFS,arguments);
-    };
-    FS.expandPath=function () {
-        return env.expandPath.apply(env,arguments);
-    };
-    FS.resolve=function (path, base) {
-        if (SFile.is(path)) return path;
-        path=env.expandPath(path);
-        if (base && !P.isAbsolutePath(path)) {
-            base=env.expandPath(base);
-            return FS.get(base).rel(path);
-        }
-        return FS.get(path);
-    };
+define(["FSLib","WebSite"],
+        function (FS,WebSite) {
+    FS.setEnv(WebSite);
     return FS;
 });
