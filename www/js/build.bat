d35c62af51e6000be1a590d1099b9c63bc01cf39
hoge1e3 <hoge1e3@gmail.com>
2016-07-01 13:18:33 +0900

    dtl build

--- www/js/build.bat
+++ www/js/build.bat
@@ -1,4 +1,5 @@
 cd /d %~dp0
+copy dolittle\lib.js ..\runtime\lib\dtl\lib.js
 copy ..\fs\home\devel\devel\JSLKer\js\concat.js ..\fs\Tonyu\Projects\JSLKer\js\concat.js
 call r_js -o build_edit.js
 call r_js -o build_selPrj.js
