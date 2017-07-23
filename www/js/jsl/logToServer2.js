b3042c3eb804eeb3a64db4083addcfe1ebf72633
hoge1e3 <hoge1e3@gmail.com>
2017-07-23 15:16:04 +0900

    prevent send raw errorParam to server(circular data9

--- www/js/jsl/logToServer2.js
+++ www/js/jsl/logToServer2.js
@@ -9,10 +9,18 @@ define([],function () {
 		code["HTML"]=codeH;
         if (detail instanceof Error) {
             var eobj={stack:detail.stack,message:detail+""};
-            for (var k in detail) eobj[k]=detail[k];
+            for (var k in detail) {
+                if (k==="errorParams") {
+                    eobj[k]=detail[k]+"";
+                } else {
+                    eobj[k]=detail[k];
+                }
+            }
             detail=eobj;
         }
-		return $.post(".?dump2",{data:JSON.stringify({date:d.getFullYear()+"/"+dataPadding(d.getMonth()+1)+"/"+dataPadding(d.getDate()),time:dataPadding(d.getHours())+":"+dataPadding(d.getMinutes())+":"+dataPadding(d.getSeconds()),lang:lang,filename:filePath,result:result,detail:detail,code:code})}).then(function (r) {
+        var data={date:d.getFullYear()+"/"+dataPadding(d.getMonth()+1)+"/"+dataPadding(d.getDate()),time:dataPadding(d.getHours())+":"+dataPadding(d.getMinutes())+":"+dataPadding(d.getSeconds()),lang:lang,filename:filePath,result:result,detail:detail,code:code};
+        //console.log("DATA",data);
+		return $.post(".?dump2",{data:JSON.stringify(data)}).then(function (r) {
 			console.log(r);
 		}).fail(function(e){
 			console.log(e);
