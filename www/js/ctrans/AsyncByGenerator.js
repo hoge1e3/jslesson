a11971af44339986931e39833a4da84bda15c14d
hoge1e3 <hoge1e3@gmail.com>
2019-03-22 15:18:44 +0900

    Revert "somewhat"
    This reverts commit 7c47ffad260e52d24c97c2a6231ff246577d945c.

--- www/js/ctrans/AsyncByGenerator.js
+++ www/js/ctrans/AsyncByGenerator.js
@@ -1,68 +1,68 @@
-"use strict";
-
 /*global self,global*/
 //define([],function () {
 (function () {
     function F(f) {
-        var src = f + "";
-        var argsp = /^function\s*\(([^\)]+)\)/;
-        var args = argsp.exec(src)[1].replace(/\s/g, "").split(",");
-        var bodyp = /\/\*---((\n|\r|.)*)---\*\//;
+        var src=f+"";
+        var argsp=/^function\s*\(([^\)]+)\)/;
+        var args=argsp.exec(src)[1].replace(/\s/g,"").split(",");
+        var bodyp=/\/\*---((\n|\r|.)*)---\*\//;
         try {
-            return Function.apply(null, args.concat([bodyp.exec(src)[1]]));
+            return Function.apply(null, args.concat([ bodyp.exec(src)[1] ]));
         } catch (e) {
             return f;
         }
     }
     // same with root.js
-    function getRoot() {
-        if (typeof window !== "undefined") return window;
-        if (typeof self !== "undefined") return self;
-        if (typeof global !== "undefined") return global;
-        return function () {
-            return this;
-        }();
+    function getRoot(){
+        if (typeof window!=="undefined") return window;
+        if (typeof self!=="undefined") return self;
+        if (typeof global!=="undefined") return global;
+        return (function (){return this;})();
     }
-    var root = getRoot();
+    var root=getRoot();
     //
-    var AsyncByGenerator = root.AsyncByGenerator = {
-        doReady: function doReady() {
-            this.isReady = true;
-            if (this.onReady) {
-                this.onReady();
-            }
+    var AsyncByGenerator=root.AsyncByGenerator={
+        doReady: function () {
+           this.isReady=true;
+           if (this.onReady) {
+               this.onReady();
+           }
         },
-        ready: function ready(f) {
+        ready: function (f) {
             if (this.isReady) {
                 f();
             } else {
-                this.onReady = f;
+                this.onReady=f;
             }
         },
         isPromise: function isPromise(v) {
-            return typeof Promise === "function" && v instanceof Promise; //(v && typeof v.then==="function");
+            return (typeof Promise==="function") && (v instanceof Promise);//(v && typeof v.then==="function");
         },
         isGenerator: function isGenerator(v) {
-            return v && (typeof Symbol === "function" && v[Symbol.toStringTag] === "Generator" || this.GeneratorFunction && this.GeneratorFunction.prototype.isPrototypeOf(v));
+            return v &&
+                ((typeof Symbol==="function" && v[Symbol.toStringTag]==="Generator") ||
+                    (this.GeneratorFunction &&
+                    this.GeneratorFunction.prototype.isPrototypeOf(v))
+                );
         },
-        init: function init() {
+        init: function () {
             try {
-                var f = new Function("this.GeneratorFunction = ((function*(){})()).constructor;");
+                var f=new Function("this.GeneratorFunction = ((function*(){})()).constructor;");
                 f.call(this);
-                this.supportsGenerator = true;
-            } catch (e) {
-                this.supportsGenerator = false;
+                this.supportsGenerator=true;
+            } catch(e) {
+                this.supportsGenerator=false;
             }
-            console.log("ABG", this.supportsGenerator);
+            console.log("ABG",this.supportsGenerator);
             this.doReady();
         },
-        run: function run(it) {
-            var t = this;
-            while (true) {
+        run: function (it) {
+            var t=this;
+            while(true) {
                 var n;
                 try {
-                    n = it.next();
-                } catch (e) {
+                    n=it.next();
+                } catch(e) {
                     return Promise.reject(e);
                 }
                 if (this.isPromise(n.value)) {
@@ -122,4 +122,3 @@
     }*/
     return root.AsyncByGenerator;
 })();
-//# sourceMappingURL=AsyncByGenerator.js.map
