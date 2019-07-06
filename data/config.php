b1e39f8a0ff757ea974a45fe7bf0f8610d967aa8
hoge1e3 <hoge1e3@gmail.com>
2019-07-06 11:02:55 +0900

    apply shadow

--- data/config.php
+++ data/config.php
@@ -3,6 +3,12 @@
 
 define("PDO_DSN","sqlite:".BA_DATA."/db/db2.sqlite3");
 define("PYTHON_PATH","C:\\bin\\python.bat");
+define("SUPER_PYTHON","superpython");
+define("PYTHON_WORK",BA_DATA."/pythonwork/");
+
+define("SHADOW_ALGO","sha256");
+define("SHADOW_SALT","hogefuga");
+
 //define("PDO_USER","");
 //define("PDO_PASS","");
 
