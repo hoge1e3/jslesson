73eb21a6a1e6cd06803a238cee75b1a7e929e758
hoge1e3 <hoge1e3@gmail.com>
2020-02-12 13:19:09 +0900

    notecontroller

---
+++ www/php/user/NoteController.php
@@ -0,0 +1,15 @@
+<?php
+req("auth");
+class NoteController {
+    public static function put() {
+        $class=Auth::curClass2();
+        $target=param("target");// varchar(32), -- externalized.id or null(root)
+        //`time` integer,
+        $time=DateUtil::now();
+        //`class` varchar(32),
+        //`user` varchar(32),
+        $file=param("file");// varchar(250), -- file path or null (for other externalized)
+        $content=param("content");// text, -- empty on just a fav
+        $favs=param("favs");// integer, -- 0 or 1?
+    }
+}
