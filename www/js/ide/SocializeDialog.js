4c644cc388280a94fee56d6dca596a64a7d31a41
hoge1e3 <hoge1e3@gmail.com>
2020-02-17 15:40:51 +0900

    realtime refresh
    fav rm は反映されない
    TODO: 間引きされていたら...

--- www/js/ide/SocializeDialog.js
+++ www/js/ide/SocializeDialog.js
@@ -15,13 +15,15 @@ define(function (require,exports,module) {
     up.queue=[];
     up.reserve=f=>up.queue.push(f);
     setInterval(up,20);
-    function CommentPopup(com) {
+    function NotePopup(note) {
+        note.favsHaving=note.favsHaving||0;
         const favButton=UI("span",{on:{click:toggleFav}},"❤");
-        if (com.favByMyself) favButton.addClass("faved");
-        const favCount=UI("span",com.favsHaving||"0");
+        if (note.favByMyself) favButton.addClass("faved");
+        const favCount=UI("span",note.favsHaving||"0");
+        const closeButton=UI("span",{on:{click:hide}},"x");
 
-        const like=UI("span",favButton, favCount).css({float:"right"});
-        const elem=UI("div",{class:"socializePopup"},com.content,like).
+        const like=UI("span",favButton, favCount, closeButton).css({float:"right"});
+        const elem=UI("div",{class:"socializePopup"},note.content,like).
         appendTo("body");
         let at;
         function show(_at) {
@@ -30,7 +32,7 @@ define(function (require,exports,module) {
                 return up.reserve(show);
             }
             popups.push(self);
-            console.log("POP!",com);
+            console.log("POP!",note);
             up.cnt=10;
             elem.show();
             elem.css({left:`${at.left}px`, top:`${at.top}px`});
@@ -44,27 +46,79 @@ define(function (require,exports,module) {
             const i=popups.indexOf(self);
             popups.splice(i,1);
         }
+        function refresh() {
+            favCount.text(note.favsHaving);
+        }
         async function toggleFav() {
-            if (com.favByMyself) {
-                await ctrl.get("Note/rmFav",{id:com.favByMyself});
-                favCount.text(favCount.text()-1);
+            if (note.favByMyself) {
+                await ctrl.get("Note/rmFav",{id:note.favByMyself});
+                note.favsHaving--;
+                favCount.text(note.favsHaving);
                 favButton.removeClass("faved");
-                com.favByMyself=false;
+                note.favByMyself=false;
             } else {
-                await ctrl.get("Note/addFav",{id:com.id});
-                favCount.text(favCount.text()-0+1);
+                const id=await ctrl.get("Note/addFav",{id:note.id});
+                //note.favsHaving++;
+                favCount.text(note.favsHaving-0+1);
                 favButton.addClass("faved");
-                com.favByMyself=true;
+                note.favByMyself=id;
+
             }
         }
-        const self={show,hide,moveBy};
+        const self={note,show,hide,moveBy,refresh};
 
         return self;
     }
-    CommentPopup.clear=()=>{
+    NotePopup.clear=()=>{
         while (popups.length) popups[0].hide();
         up.queue=[];
     };
+    const notesByFiles={};// filepath->NotesByFile
+    class NotesByFile{
+        //filepath:string
+        //last:time;
+        //entries:{id->note} ;
+        constructor (filepath) {
+            this.filepath=filepath;
+            this.entries={};
+            this.last=0;
+            this.lastRefresh=0;
+        }
+        addEntry(note) {
+            this.entries[note.id]=this.entries[note.id]||{};
+            note.favsHaving+=this.entries[note.id].favsHaving||0;
+            Object.assign(this.entries[note.id], note);
+            if (note.time>this.last) this.last=note.time;
+            if (note.c_time>this.last) this.last=note.c_time;
+        }
+        async refresh(duration=0) {
+            const now=new Date().getTime();
+            if (now<this.lastRefresh+duration) return false;
+            this.lastRefresh=now;
+            const notes=await ctrl.get("Note/get",{file:this.filepath,since:this.last});
+            console.log(notes);
+            for (let note of notes) {
+                this.addEntry(note);
+            }
+            return true;
+        }
+        pick(except) {
+            except=except||{};
+            const cand=[];
+            for (let id in this.entries) {
+                if (!except[id]) cand.push(this.entries[id]);
+            }
+            if (cand.length==0) return null;
+            const a=cand[Math.floor(Math.random()*cand.length)];
+            const b=cand[Math.floor(Math.random()*cand.length)];
+            if (a.favsHaving>b.favsHaving) return a;
+            return b;
+        }
+        static instance(filepath) {
+            notesByFiles[filepath]=notesByFiles[filepath]|| new NotesByFile(filepath);
+            return notesByFiles[filepath];
+        }
+    }
     module.exports=(ide)=>{
         const elem=UI("div",{title:"についてのノート"},
             ["input", {$var:"cont", size:60,on:{enterkey:send}}],
@@ -78,33 +132,56 @@ define(function (require,exports,module) {
             const o=elem.offset();
             console.log(o);
             o.top-=50;
-            const com={content:cont};
-            CommentPopup(com).show(o);
+            const note={content:cont};
             if (file) {
                 const prjTop=ide.prj.getDir().up();
                 const filePath=file.relPath(prjTop);
-                com.id=await ctrl.post("Note/put",{
+                note.id=await ctrl.post("Note/put",{
                     file:filePath,
                     content:cont,
                 });
-                console.log("New note ",com.id);
+                NotePopup(note).show(o);
+                console.log("New note ",note.id);
             }
         }
+        let timer;
         async function changeFile(f) {
             if (file && f && file.path()===f.path()) return;
-            CommentPopup.clear();
+            NotePopup.clear();
+            if (timer) clearTimeout(timer);
             file=f;
             const prjTop=ide.prj.getDir().up();
             const filePath=file.relPath(prjTop);
 
-            elem.dialog("option","title",f.name()+"について気づいたこと");
-            const coms=await ctrl.get("Note/get",{file:filePath});
+            elem.dialog("option","title",f.name()+"についてのノート");
+            const n=NotesByFile.instance(filePath);
+            await n.refresh();
+            async function loop() {
+                if (await n.refresh(10*1000)) {
+                    for (let c of popups) c.refresh();
+                }
+                //console.log("EX",except);
+                if (popups.length<5) {
+                    const except={};
+                    for (let c of popups) except[c.note.id]=c;
+                    const com=n.pick(except);
+                    if (com) {
+                        const o=elem.offset();
+                        o.top-=50;
+                        NotePopup(com).show(o);
+                    }
+                }
+                timer=setTimeout(loop,1000);
+            }
+            loop();
+
+            /*const coms=await ctrl.get("Note/get",{file:filePath});
             console.log("coms",coms);
             for (let com of coms) {
                 const o=elem.offset();
                 o.top-=50;
                 CommentPopup(com).show(o);
-            }
+            }*/
         }
         function show(f) {
             elem.dialog({width:800,height:100,position: { my: "center bottom", at: "center bottom"},});
--- www/php/user/NoteController.php
+++ www/php/user/NoteController.php
@@ -64,28 +64,35 @@ class NoteController {
         */
         if ($file) {
             $rf=pdo_select("select n1.*, ".
-                "n2.id as c_id, ".
+                "n2.id as c_id, n2.time as c_time, ".
                 "n2.class as c_class, n2.user as c_user, ".
                 "n2.content as c_content, n2.favs as c_favs ".
                 "from ".
                 "note n1 left join note n2 ".
                 "on n2.target=n1.id ".
-                "where n1.class=? and n1.file=? and n1.time>? ",
-                $user->_class->id,$file,$since);
-            $r=array();
+                "where n1.content<>'' and n1.class=? and n1.file=? and (n1.time>? or n2.time>?)",
+                $user->_class->id,$file,$since,$since);
+            $rh=array();
             $ra=array();
             foreach ($rf as $e) {
-                if (!isset($r[$e->id])) {
-                    $r[$e->id]=$e;
-                    $r[$e->id]->favsHaving=0;
-                    array_push($ra,$e);
+                if (isset($rh[$e->id])) {
+                    $r=$rh[$e->id];
+                } else {
+                    $r=$e;
+                    $rh[$e->id]=$r;
+                    $r->favsHaving=0;
+                    $r->commentsHaving=array();
+                    array_push($ra,$r);
                 }
                 if ($e->c_favs) {
-                    $r[$e->id]->favsHaving+=$e->c_favs;
+                    $r->favsHaving+=$e->c_favs;
                     if ($e->c_class===$user->_class->id &&
                         $e->c_user===$user->name ) {
-                            $r[$e->id]->favByMyself=$e->c_id;
+                        $r->favByMyself=$e->c_id;
                     }
+                }
+                if ($e->c_content) {
+                    array_push($r->commentsHaving, $e);
                 }
             }
             header("Content-type: text/json");
@@ -109,6 +116,7 @@ class NoteController {
             //"content"=>$content,
             "favs"=>$favs
         ));
+        echo $id;
     }
     public static function rmFav() {
         $id=param("id");
