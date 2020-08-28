d71d0061c64965e39e098431e9d1fe52145a5058
hoge1e3 <hoge1e3@gmail.com>
2020-08-28 15:00:21 +0900

    Test error catch

---
+++ www/js/lib/EventHandler.js
@@ -0,0 +1,40 @@
+define(function (require, exports, module) {
+    class SingleTypeEventHandler {
+        constructor(type) {
+            this.funcs=[];
+            this.type=type;
+        }
+        on(...args) {
+            const f=args.pop();
+            if (typeof f!=="function") {
+                console.log(args, f);
+                throw new Error("Not a function for type "+this.type);
+            }
+            this.funcs.push(f);
+            const r={
+                remove: ()=>{
+                    const e=this.funcs.indexOf(f);
+                    if (e>=0) this.funcs.splice(e,1);
+                }
+            };
+            return r;
+        }
+        fire(...args) {
+            for (let f of this.funcs) f(...args);
+        }
+    }
+    class EventHandler {
+        constructor() {
+            this.types={};
+        }
+        on(type, ...args) {
+            this.types[type]=this.types[type]||new SingleTypeEventHandler(type);
+            return this.types[type].on(...args);
+        }
+        fire(type,...args) {
+            this.types[type]=this.types[type]||new SingleTypeEventHandler(type);
+            return this.types[type].fire(...args);
+        }
+    }
+    module.exports=EventHandler;
+});
