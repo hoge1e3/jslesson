7dfcb055b92a56a0a78e3e4ed780ae6c7287e661
hoge1e3 <hoge1e3@gmail.com>
2021-07-29 17:28:49 +0900

    ip

--- www/php/dump2.php
+++ www/php/dump2.php
@@ -6,6 +6,12 @@ $user=Auth::curUser();
 $class=Auth::curClass();
 $data=$_POST["data"];
 $fp=fopen(BA_LOG."/$class-$user-data.log","a");
+if (isset( $_SERVER["REMOTE_ADDR"] )) {
+    $o=json_decode($data);
+    $o->ip=$_SERVER["REMOTE_ADDR"];
+    $data=json_encode($o);
+}
+
 $time=date(DATE_ATOM);
 
 fwrite($fp, "$data\n");
