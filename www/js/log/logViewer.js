772f5a24530e47497ae76542a4a70c6ee4ae4d4d
hoge1e3 <hoge1e3@gmail.com>
2023-04-16 17:38:33 +0900

    userNameOf

--- www/js/log/logViewer.js
+++ www/js/log/logViewer.js
@@ -370,12 +370,12 @@ function openFrame(data){
       <div><a target='raw' href="${rawLink}">Raw..</a></div>`+
       (runLink ?
           "<a target='runCheck' href='"+runLink+"'>実行してみる</a><br>":"")+
-      `<span class="userid">${userid}</span>(<span id='userName'></span>)<BR>`+
+      `<span class="userid">${userid}</span>(<span id='userNameOf${userid}'></span>)<BR>`+
       filehist+ navByFile(data.filename)+
       `actualTime=<span class='actualTime'>${logDOM.attr("data-actualTime")}</span>`+"<br>"+
       `actualTime2=<span class='actualTime2'>${logDOM.attr("data-actualTime2")}</span>`+"<br>"+
       data.result);
-  $.get("?TeacherLog/getNameOfUser",{user:userid}).then(r=>$("#userName").text(r));
+  $.get("?TeacherLog/getNameOfUser",{user:userid}).then(r=>$("#userNameOf"+userid).text(r));
   $("[id='"+userid+"']").height(30);
   $("[id='"+userid+"']").html(res);
   $("[id='"+userid+"']").css("display","inline");
