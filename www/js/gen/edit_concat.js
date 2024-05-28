84ad059fdc4df0e25783e0923a9b87bf07727dc6
hoge1e3 <hoge1e3@gmail.com>
2024-05-28 11:40:46 +0900

    Firefox ALWAYS_UPLOAD

--- www/js/gen/edit_concat.js
+++ www/js/gen/edit_concat.js
@@ -17182,9 +17182,9 @@ function ready() {
         setupBuilder(_);
     });
     helpURL=langInfo.helpURL;
-    if (navigator.userAgent.match(/Firefox/) && lang==="tonyu") {
+    if (navigator.userAgent.match(/Firefox/) /*&& lang==="tonyu"*/) {
         ALWAYS_UPLOAD=true;
-        console.log("Firefox tonyu ALWAYS_UPLOAD");
+        console.log("Firefox ALWAYS_UPLOAD");
     }
     function setupBuilder(BuilderClass) {
         $("#fullScr").attr("href",JS_NOP).text("別ページで表示");
