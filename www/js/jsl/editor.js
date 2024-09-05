62f77ff4e3d7d4b0baab19cbed477978c7a58747
hoge1e3 <hoge1e3@gmail.com>
2024-09-05 14:32:30 +0900

    build

--- www/js/jsl/editor.js
+++ www/js/jsl/editor.js
@@ -131,6 +131,8 @@ define(function (require) {
         if (info.BA_SERVICE_URL) {
             WebSite.controller_in_service=info.BA_SERVICE_URL;
             WebSite.runtime_in_service=FS.PathUtil.truncSEP(info.BA_SERVICE_URL)+"/runtime/";
+        } else {
+            WebSite.runtime_in_service=WebSite.runtime;
         }
         if (info.BA_PUB_URL) {
             // BitArrow.publishedURL: URL of THIS project.
