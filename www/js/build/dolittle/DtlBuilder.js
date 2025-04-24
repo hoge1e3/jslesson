1885bf8cd7987f62027c1f81d36888a9960f724a
hoge1e3 <hoge1e3@gmail.com>
2025-04-24 11:43:38 +0900

    in progress

--- www/js/build/dolittle/DtlBuilder.js
+++ www/js/build/dolittle/DtlBuilder.js
@@ -4,55 +4,10 @@ function (A,DU,wget,dtlParser,IndentBuffer,Sync,FS,SplashScreen,ABG,UI,root,WebS
         this.prj=prj;// TPRC
         this.dst=dst;// SFile in ramdisk
     };
-    /*var images=["99.gif", "akazukin.gif", "apple.png", "arrow0.png",
-    "arrow1.png", "arrow2.png", "arrow3.png", "ayumi.gif",
-    "ayumi.png", "ayumiAka.gif", "ayumiAo.gif", "ayumiBlue.png",
-    "ayumiKiiro.gif", "ayumiRed.png", "ayumiYellow.png",
-    "ball.png", "base.png", "beetle.png", "bluefish.png",
-    "book.png", "car.png", "clear.png", "copy.png",
-    "crab.png", "cut.png", "ecl.png", "editAdd.png", "fish.png",
-    "heri.gif", "inputPad.png", "kuno.gif", "kuno.png",
-    "mapchip.png", "myurobo.png", "neko.png", "neko1.png",
-    "neko2.png", "niwa.gif", "nowprint.png", "paste.png",
-    "pen.png", "rabbitBlue.png", "rabbitGreen.png",
-    "rabbitRed.png", "rabbitYellow.png", "raceBlue.png",
-    "raceGreen.png", "raceRed.png", "raceYellow.png",
-    "redo.png", "rocket.gif", "runner.png", "Sample.png",
-    "server.png", "soccer.png", "sound.png", "star.png",
-    "tnu.ico", "tonbo.gif", "tonyu.png", "trumpet.png",
-    "tulip.png", "ui-icons_888888_256x240.png", "undo.png", "usa.gif"].map(
-        function (n) {return "images/"+n;});*/
-    var libs=["polyfill","jquery-1.12.1","require","AsyncByGenerator"].map(function (n) {
-        return "lib/"+n+".js";
-    });
-    /*var dtlibs=["promise","mt","lib",
-    "parser","ExpressionParser","context","minimal",
-    "polyk","calibration","devicemotion",
-    "Dict","Vec2","Actor","Group","UI","Color","Timer",
-    "Util","Turtle","Figure","DOM","TextFile","Japanese2","db","Ajax"].map(
-        function (n) {
-            return "lib/dtl/"+n+".js";
-        }
-    );*/
     var p=DtlBuilder.prototype;
     p.progress=function (m) {
         if (window.SplashScreen) window.SplashScreen.progress(m);
     };
-    /*p.dlFiles=function () {
-        var dst=this.dst;
-        var urls=[];
-        //"lib/jquery-1.12.1.js",
-        //"lib/require.js","lib/dtl/lib.js","lib/dtl/polyk.js"];
-        urls=urls.concat(libs);
-        urls=urls.concat(dtlibs);
-        //urls=urls.concat(images);
-        var base="runtime/";
-        var args=urls.map(function (url) {
-            var dstf=dst.rel(url);
-            if (!dstf.exists()) return wget(base+url, dstf);
-        });
-        return $.when.apply($,args);
-    };*/
     p.genHTML=function (f) {
         this.progress("generate "+f.src.html.name());
         //var curHTMLFile=d.rel(name+".html");
@@ -68,7 +23,7 @@ function (A,DU,wget,dtlParser,IndentBuffer,Sync,FS,SplashScreen,ABG,UI,root,WebS
                 version:BitArrow.version,
                 urlArgs:BitArrow.urlArgs,
                 publishedURL:BitArrow.publishedURL,
-                runtimePath:WebSite.runtime,
+                runtimePath:WebSite.runtime_in_service||WebSite.runtime,
                 serverTop: WebSite.serverTop,
                 main:f.name};
             $(head).append($("<script>").text("window.BitArrow="+JSON.stringify(ba)+";"));
@@ -90,46 +45,6 @@ function (A,DU,wget,dtlParser,IndentBuffer,Sync,FS,SplashScreen,ABG,UI,root,WebS
         });
         return f.dst.html.text("<!DOCTYPE HTML>\n<html>"+html.innerHTML+"</html>");
     };
-    /*p.genHTMLOLD=function (f) {
-        this.progress("generate "+f.src.html.name());
-        //var curHTMLFile=d.rel(name+".html");
-        var dp=new DOMParser();
-        var dom=dp.parseFromString(f.src.html.text(),"text/html");
-        var html=dom.getElementsByTagName("html")[0];
-        var head=dom.getElementsByTagName("head")[0];
-        var body=dom.getElementsByTagName("body")[0];
-        $(head).append($("<meta>").attr("charset","UTF-8"));
-        if (window.BitArrow) {
-            var BitArrow=window.BitArrow;
-            var ba={
-                version:BitArrow.version,
-                urlArgs:BitArrow.urlArgs,
-                publishedURL:BitArrow.publishedURL,
-                runtimePath:WebSite.runtime};
-            $(head).append($("<script>").text("window.BitArrow="+JSON.stringify(ba)+";"));
-        }
-        $(head).append($("<script>").text("window.runtimePath='"+WebSite.runtime+"';"));
-        $(head).append($("<script>").text("window.onerror=window.onerror||"+
-        function (e) {alert(e);}+";"));
-        $(head).append($("<link>").attr({"rel":"stylesheet","href":WebSite.runtime+"css/run_style.css"}));
-
-        libs.concat(dtlibs).map(function (r) {
-            return WebSite.runtime+r;
-        }).concat([f.name+".js"]).forEach(function (src) {
-            var nn=document.createElement("script");
-            nn.setAttribute("charset","utf-8");
-            var src2;
-            var requirejs=root.requirejs;
-            if (FS.PathUtil.isURL(src) && requirejs.version!=="2.1.9" && typeof requirejs.s.contexts._.config.urlArgs==="function") {
-                src2=src+requirejs.s.contexts._.config.urlArgs("",src);
-            } else {
-                src2=src+(src.indexOf("?")<0?"?":"&")+Math.random();
-            }
-            nn.setAttribute("src",src2);
-            body.appendChild(nn);
-        });
-        return f.dst.html.text("<!DOCTYPE HTML>\n<html>"+html.innerHTML+"</html>");
-    };*/
     function isNewer(a,b) {
         if (!a.exists()) return false;
         return a.lastUpdate()>b.lastUpdate();
