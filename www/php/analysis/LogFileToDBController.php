cff6d71fc0ef7e6da86343519811139e2a39db9b
hoge1e3 <hoge1e3@gmail.com>
2025-08-16 11:39:55 +0900

    lang filed only length 10

--- www/php/analysis/LogFileToDBController.php
+++ www/php/analysis/LogFileToDBController.php
@@ -146,14 +146,18 @@ class LogFileToDBController {
                     }
                     //echo DateUtil::toString($time);
                     $a=array(
-            	        $time,$c,$user,$lang,substr($filename,0,250),
+            	        $time,$c,$user,
+                        substr($lang,0,9),
+                        substr($filename,0,250),
                         $result,
                         mb_substr($detail,0,20000),
                         mb_substr($raw,0,20000),$errorType,$errorPos
         	        );
                     //var_dump($a);
             	    $sth->execute($a);
-                } catch(Exception $e) {}
+                } catch(Throwable $e) {
+                    error_log($e->getMessage());
+                }
     	        //break;
             }
             $file->appendTo($arc->rel($file->name()) );
